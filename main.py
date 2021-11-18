5455db5bc7602774127d6b1837928b0263a44b16
samsonovv <samsonov.vs@phystech.edu>
2021-11-18 19:02:52 +0300

    Введена переменная тип экрана для разбиения программы модули

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
+import pygame
+from pygame.draw import *
+pygame.init()
+screen_type = 'menu'
 
+FPS = 30
+screen = pygame.display.set_mode((1600, 900))
+
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+
+pygame.display.update()
+clock = pygame.time.Clock()
+finished = False
+
+while not finished:
+    clock.tick(FPS)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            finished = True
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            if screen_type == 'menu':
+                print("Андрей")
+
+            if screen_type == 'gameplay':
+
+                print("Сом")
+
+pygame.quit()
