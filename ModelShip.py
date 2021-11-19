0d7b14a9d5ce8ecf37d96dae5a5121fdc273e53a
Vladimir-Abrosimov <abrosimov.vn@phystech.edu>
2021-11-19 11:58:58 +0400

    2

--- ModelShip.py
+++ ModelShip.py
@@ -16,7 +16,7 @@ TURNFORCE = [10, 20, 30]
 MASS = [1000, 2000, 3000]
 
 class GameShip:
-    def _init_(self):
+    def __init__(self, m, x, y, alpha, V, gameXP, color, F, TF, dt):
         self.m = choice(MASS)
         self.x = 450
         self.y = 20
@@ -26,7 +26,14 @@ class GameShip:
         self.color = choice(COLOR)
         self.F = choice(FORCE)
         self.TF = choice(TURNFORCE)
+        self.dt = 0.1
+        image = pygame.image.load('Bismark_side-removebg-preview.png').convert_alpha()
+        new_image_0 = pygame.transform.scale(image, (image.get_width() * 0.5, image.get_height() * 0.5))
+        self.new_image = pygame.transform.rotate(new_image_0, (alpha + pi/2) * 360 * (2 * pi) ** -1)
 
-    def
 
 
+    def Move(self, m, x, y, alpha, V, gameXP, color, F, TF):
+        self.x += (F / b - (F / b - self.V * sin(self.alpha)) * exp((-b / m) * (t - t0))) * self.dt
+        self.y += (self.V * cos(self.alpha)  - (TF / m) * (t - t0)) * self.dt
+        pass
