3291707f72053a37dab316574b6c9a3ead66f3a3
samsonovv <samsonov.vs@phystech.edu>
2021-11-21 01:34:08 +0300

    Добавлено дофига всего, собран каркас, написаны комментарии

--- gameplay.py
+++ gameplay.py
@@ -1,12 +1,12 @@
-from Torped import *
+from Torped_classes import *
 import pygame
 from ModelShip import *
 from pygame.gfxdraw import *
 
 class Gameplay:
     def __init__(self, pl1, pl2):
-        self.torpeds = [Torped]
-        self.time = 300
+        self.torpeds = []
+        self.time = 0
         pygame.font.init()
         # Экземпляры кораблей,  свойства которых надо устанавливать при нажатии Play
         self.leftPl: GameShip = pl1
@@ -20,28 +20,29 @@ class Gameplay:
     def drawInfo(self, screen):
         red_name_lable = self.font2.render("Bismark", True, (255, 255, 255), None)
         screen.blit(red_name_lable, (30, 30))
-        green_name_lable = self.font2.render("Yowa", True, (255, 255, 255), None)
+        green_name_lable = self.font2.render("Iowa", True, (255, 255, 255), None)
         screen.blit(green_name_lable, (1315, 30))
 
     def drawTime(self, screen):
-        seconds = str(self.time % 60)
+        t = self.time // 60
+        seconds = str(t % 60)
         if seconds == '0':
             seconds = '00'
-        time_label = self.font1.render(str(self.time // 60) + ":" + seconds, True, (255, 255, 255), None)
+        time_label = self.font1.render(str(t // 60) + ":" + seconds, True, (255, 255, 255), None)
         screen.blit(time_label, (750, 30))
         pass
 
     def drawTorpeds(self, screen):
-        for i in self.torpeds:
-            if (i.x <= 1600) and (i.x >= 0) and (i.y <= 900) and (i.y >= 0):
-                i.moveTorped()
-                if i.Hit():
-                    i.detonation()
-                    self.torpeds.remove(i)
+        for i in range(0, len(self.torpeds)):
+            if (self.torpeds[i].x <= 1600) and (self.torpeds[i].x >= 0) and (self.torpeds[i].y <= 900) and (self.torpeds[i].y >= 0):
+                self.torpeds[i].moveTorped()
+                if self.torpeds[i].Hit(screen):
+                    self.torpeds[i].detonation(screen)
+                    self.torpeds.remove(self.torpeds[i])
                 else:
-                    screen.blit(i.new_image, (i.x, i.y))
+                    screen.blit(self.torpeds[i].new_image, (self.torpeds[i].x, self.torpeds[i].y))
             else:
-                self.torpeds.remove(i)
+                self.torpeds.remove(self.torpeds[i])
 
 # Класс для отрисовки в геймплее индикаторов торпедных аппаратов
 class torped_recharge_units:
--- main.py
+++ main.py
@@ -18,26 +18,57 @@ pygame.display.update()
 clock = pygame.time.Clock()
 finished = False
 
+# Это над для создания класса геймплея - не трогать и не смотреть
+Left_player = GameShip(100,1,1,1,0,10,10, (0,0,0),10,10,0.01)
+Right_player = GameShip(100,1,1,1,0,10,10, (0,0,0),10,10,0.01)
+
+# создание классов игровых экранов - меню и и геймплея
+gameplay_screen = Gameplay(Left_player, Right_player)
+# Экран меню Андрея
+
+
 # это тоже код для проверки
 ship = GameShip(1000, 0.01, 450, 800, 0, 10, 100, (1, 1, 1), 100, 10, 0.1)
+
 while not finished:
+
+    if screen_type == 'menu':
+        gameplay_screen.time = 0
+
+    if screen_type == 'gameplay':
+        gameplay_screen.time -= 1
+        gameplay_screen.drawTime(screen)
+        gameplay_screen.drawInfo(screen)
+
     clock.tick(FPS)
+    pygame.display.update()
+    screen.fill(sea_color)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             finished = True
         else:
             if screen_type == 'menu':
-                # код Андрея для отрисовки меню и работы с кнопкой Play
-                print('menu')
+                # просто код чтобы тестировать - можно стереть
+                if event.type == pygame.KEYDOWN and event.key == pygame.K_w:
+                    screen_type = 'gameplay'
+                    gameplay_screen.time = 300*FPS
 
             if screen_type == 'gameplay':
-                # Код Сома для отрисовки геймплея
-                print('gameplay')
-
-            # код Вовы для проверки - можно стирать
-            pressed_keys = pygame.key.get_pressed()
-            if pressed_keys[pygame.K_w]:
-                print("ffw")
-                ship.MoveForward(pi, screen)
-    pygame.display.update()
+                if event.type == pygame.KEYDOWN and event.key == pygame.K_w:
+                    """ добавление тяги у gameplay_screen.leftPl"""
+                if event.type == pygame.KEYDOWN and event.key == pygame.K_s:
+                    """ убавление тяги у gameplay_screen.leftPl"""
+                if event.type == pygame.KEYDOWN and event.key == pygame.K_d:
+                    """ правее gameplay_screen.leftPl"""
+                if event.type == pygame.KEYDOWN and event.key == pygame.K_a:
+                    """ левее gameplay_screen.leftPl"""
+                if event.type == pygame.KEYDOWN and event.key == pygame.K_i:
+                    """добавление тяги у gameplay_screen.righPl"""
+                if event.type == pygame.KEYDOWN and event.key == pygame.K_k:
+                    """ убавление тяги у gameplay_screen.righPl"""
+                if event.type == pygame.KEYDOWN and event.key == pygame.K_l:
+                    """ правее gameplay_screen.righPl"""
+                if event.type == pygame.KEYDOWN and event.key == pygame.K_j:
+                    """ левее gameplay_screen.righPl"""
 pygame.quit()
