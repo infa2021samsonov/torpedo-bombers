f432efb7e739e0b790983355735a40790b0ff9fa
infa2021samsonov <samsonov.vs@phystech.edu>
2021-11-25 14:49:48 +0300

    Вовин код добавлен

--- ModelShip.py
+++ ModelShip.py
@@ -16,36 +16,75 @@ TURNFORCE = [10, 20, 30]
 MASS = [1000, 2000, 3000]
 
 class GameShip:
-    def __init__(self, m, b, x, y, alpha, V, gameXP, color, F, TF, dt, quantity_of_torpeds, recharge_time, maxXP):
+    def __init__(self, m, b, x, y, alpha, V, omega, gameXP, color, F, TF, dt, maxXP, quantity_of_torpeds, recharge_time):
         self.maxXP = maxXP
         self.m = choice(MASS)
         self.b = 0.01
         self.x = 450
         self.y = 20
         self.alpha = 0
-        self.V = 100
+        self.V = 10
+        self.omega = 10
         self.gameXP = maxXP/2
         self.color = choice(COLOR)
         self.F = choice(FORCE)
         self.TF = choice(TURNFORCE)
         self.dt = 0.1
+        self.kw = False
+        self.ks = False
+
+        self.ka = False
+        self.kd = False
         # Количество торпедных аппаратов у данного корабля и время их перезарядки
         self.quantity_of_torpeds = quantity_of_torpeds
         self.recharge_time = recharge_time
         # создание массива в котором хранится время последнего заряжания этого i-го торпедного аппарата
         self.torped_tubes = []
         for i in range(0, self.quantity_of_torpeds):
-            self.torped_tubes.append(300*60)
+            self.torped_tubes.append(300 * 60)
         # это сделано Сомом для отображения
 
+    def keyinput(self, event):
+        if event.type == pygame.KEYDOWN:
 
-    def MoveForward(self,alpha, screen):
-        image_1 = pygame.image.load('/torpedo-bombers/Bismark_top-removebg-preview.png')
-        new_image_0 = pygame.transform.scale(image_1, (int(image_1.get_width() * 0.5), int(image_1.get_height() * 0.5)))
-        self.new_image = pygame.transform.rotate(new_image_0, (alpha + math.pi / 2) * 360 * (2 * math.pi) ** -1)
+            if event.key == pygame.K_w:
+                self.kw = True
+            if event.key == pygame.K_s:
+                self.ks = True
+            if event.key == pygame.K_a:
+                self.ka = True
+            if event.key == pygame.K_d:
+                self.kd = True
+
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_w:
+                self.kw = False
+            if event.key == pygame.K_s:
+                self.ks = False
+            if event.key == pygame.K_a:
+                self.ka = False
+            if event.key == pygame.K_d:
+                self.kd = False
+
+    def DrawShip(self, screen):
+        image = pygame.image.load('/Users/samsonovv/torpedo-bombers/IOWA_top-removebg-preview.png').convert_alpha()
+        new_image_0 = pygame.transform.scale(image, (int(image.get_width() * 0.15), int(image.get_height() * 0.15)))
+        self.new_image = pygame.transform.rotate(new_image_0, (self.alpha + math.pi / 2) * 360 * (2 * math.pi) ** -1)
         screen.blit(self.new_image, (self.x, self.y))
-        self.x += (self.F / self.b - (self.F / self.b - self.V * math.sin(self.alpha)) * math.exp((-self.b / self.m) * 0.5)) * self.dt
         pass
+
+    def Move(self):
+        if self.kw == True:
+            self.x = self.x + self.V * math.cos(self.alpha) * self.dt
+            self.y = self.y + self.V * math.sin(self.alpha) * self.dt
+        if self.ks == True:
+            self.x = self.x - self.V * math.cos(self.alpha) * self.dt
+            self.y = self.y - self.V * math.sin(self.alpha) * self.dt
+        if self.ka == True:
+            self.alpha = self.alpha + self.omega * self.dt
+        if self.kd == True:
+            self.alpha = self.alpha - self.omega * self.dt
 
 
 
